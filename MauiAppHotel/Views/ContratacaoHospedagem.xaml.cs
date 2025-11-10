ce326ca98b5380bff07819707971738b3d504f4f
theo <theosouzaescola@gmail.com>
2025-11-09 21:31:59 -0300

    Atualizações app

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -3,10 +3,20 @@ namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
+    App PropriedadeApp;
 	public ContratacaoHospedagem()
 	{
 		InitializeComponent();
-	}
+
+        PropriedadeApp=(App)Application.Current;
+        pck_quarto.ItemsSource = PropriedadeApp.lista_quartos;
+
+        dtpck_checkin.MinimumDate=DateTime.Now;
+        dtpck_checkin.MaximumDate=new DateTime(DateTime.Now.Year,DateTime.Now.Month+1,DateTime.Now.Day);
+
+        dtpck_checkout.MinimumDate=dtpck_checkin.Date.AddDays(1);
+        dtpck_checkout.MaximumDate=dtpck_checkin.Date.AddMonths(6);
+    }
 
     private async void BtnSobre_Clicked(object sender, EventArgs e)
     {
@@ -29,5 +39,15 @@ public partial class ContratacaoHospedagem : ContentPage
         {
             DisplayAlert("Ops",ex.Message,"OK");
         }
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        DatePicker elemento = sender as DatePicker;
+
+        DateTime data_selecionada_checkin = elemento.Date;
+
+        dtpck_checkout.MinimumDate=data_selecionada_checkin.AddDays(1);
+        dtpck_checkout.MaximumDate=data_selecionada_checkin.AddMonths(6);
     }
 }
